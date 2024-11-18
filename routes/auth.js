e7d234b13c7c3638568d4c297250c558d7280679
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-18 15:40:17 +0530

    Added Login route

--- routes/auth.js
+++ routes/auth.js
@@ -15,3 +15,28 @@ router.post('/signup', async (req, res) => {
         res.status(500).json({ message: 'Error registering user', error: error.message});
     }
 });
+
+//Login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({username});
+        if(!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if(!passwordMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        //JWT token
+        const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+        });
+        res.status(200).json({ message: 'Login successful', token });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error Logging in', error: error.message });
+    }
+});
