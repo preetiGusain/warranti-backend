d47eda96f4d9f2e3fbc1001947726baeb69e5404
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-18 22:18:08 +0530

    Modified userSchema for  google Oauth

--- models/users.js
+++ models/users.js
@@ -2,15 +2,36 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    email: {
+        type: String,
+        unique: true,
+    },
+    name: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    googleId: String,
+    secret: String,
 });
 
+//Hashing the password
 userSchema.pre('save', async function (next) {
-    if(this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
+    const user = this;
+    if (!user.password) {
+        next();
     }
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
     next();
-})
+});
+
+//Matching the password
+userSchema.methods.isValidPassword = async function (password) {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
+};
 
 module.exports = mongoose.model('User', userSchema);
