44d80359ce35c27abe4b14f206f8c8d4d7fa0961
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-18 22:38:49 +0530

    Added check login route

---
+++ routes/api/check_login.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    if (!req.user) {
+        throw new Error('User not authenticated!');
+    }
+    res.json(req.user);
+})
+
+module.exports(router);
