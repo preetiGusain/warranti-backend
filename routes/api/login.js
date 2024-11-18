4d1b3a26aaf562074e374ed98183478072d88f2e
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-18 22:49:12 +0530

    Modified login route

---
+++ routes/api/login.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+
+router.post('/', async (req, res, next) => {
+    passport.authenticate('login', async (error, user, info) => {
+        try {
+            if (error) {
+                return res.status(500).json({
+                    message: 'Something went wrong',
+                    error: error || 'Internal server error',
+                });
+            }
+            req.login(user, async (error) => {
+                if (error) {
+                    res.status(500).json({
+                        message: 'Something went wrong',
+                        error: error || 'Internal server error',
+                    })
+                }
+                return res.json({ user, info });
+            });
+        } catch (error) {
+            return next(error);
+        }
+        (req, res, next);
+    })
+});
+
+router.get('/fail', (req, res) => {
+    res.status(401).json({
+        message: 'User failed to authenticate'
+    });
+});
+
+module.exports(router);
