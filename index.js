d2fce1e13721e60e8123413926dbf83ac5a8112f
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-18 18:26:23 +0530

    Defined simple Google oauth2 route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
+const connectDB = require('./config/db');
 
 dotenv.config();
 const app = express();
@@ -14,6 +14,14 @@ app.use(express.json());
 
 //Routes
 app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+app.get('/protected', (req, res) => {
+    res.send('Hello!');
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
