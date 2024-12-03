8d89abe756f4513d1a6400fb43bf60dc021dadc2
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-12-03 22:30:07 +0530

    setup https for production

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.use(
         resave: false,
         saveUninitialized: false,
         cookie: {
-            secure: process.env.NODE_ENV === 'production',
+            secure: true,
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
         },
