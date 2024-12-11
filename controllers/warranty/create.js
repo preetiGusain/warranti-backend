f0a8b24b4b11734839c6883b73bf2e00eb93a60f
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-12-11 14:54:21 +0530

    Storing jwt token for creating warranty

--- controllers/warranty/create.js
+++ controllers/warranty/create.js
@@ -1,5 +1,6 @@
 const Warranty = require("../../models/warranty");
 const { uploadFileToSupabase } = require("../../services/uploadFileToSupabase");
+const { getTokenResponse } = require("../utils/getTokenResponse");
 
 /**
  * @desc    Creates warranty for the customer
@@ -8,9 +9,13 @@ const { uploadFileToSupabase } = require("../../services/uploadFileToSupabase");
  */
 exports.create = async (req, res, next) => {
     try {
-        const warrantyCard = req.files['warrantyCard'][0];
-        const receipt = req.files['receipt'][0];
-        const product = req.files['product'][0];
+        // Allowing warranty images to be null temporarily
+        // const warrantyCard = req.files['warrantyCard'][0];
+        // const receipt = req.files['receipt'][0];
+        // const product = req.files['product'][0];
+        const warrantyCard = req.files['warrantyCard'] ? req.files['warrantyCard'][0] : null;
+        const receipt = req.files['receipt'] ? req.files['receipt'][0] : null;
+        const product = req.files['product'] ? req.files['product'][0] : null;
         const { productName, warrantyDuration, warrantyDurationUnit, purchaseDate, productId } = req.body;
 
         const warranty = new Warranty({
@@ -25,24 +30,26 @@ exports.create = async (req, res, next) => {
 
         let update = {};
 
-        if(warrantyCard) {
+        if (warrantyCard) {
             const warrantyCardPhotoURL = await uploadFileToSupabase(warrantyCard, req.user._id, warranty._id, "warrantyCard");
             update['warrantyCardPhoto'] = warrantyCardPhotoURL;
         }
-        if(receipt) {
+        if (receipt) {
             const receiptPhotoURL = await uploadFileToSupabase(receipt, req.user._id, warranty._id, "receipt");
             update['receiptPhoto'] = receiptPhotoURL;
         }
-        if(product) {
+        if (product) {
             const productPhotoURL = await uploadFileToSupabase(product, req.user._id, warranty._id, "product");
             update['productPhoto'] = productPhotoURL;
         }
-        
-        const updatedWarranty = await Warranty.findByIdAndUpdate(savedWarranty._id, update, {new: true});
 
-        res.status(200).json({message: "Warranty created successfully", updatedWarranty});
+        const updatedWarranty = await Warranty.findByIdAndUpdate(savedWarranty._id, update, { new: true });
+        res.status(200).json({ message: "Warranty created successfully", updatedWarranty });
+
+        getTokenResponse(req.user, 200, res);
+        
     } catch (error) {
-        console.error(error);
-        res.status(500).json({message: 'Server error'});
+        console.error('Error in creating warranty:', error);
+        res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
